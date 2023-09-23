bfd7929368150e68a13a3ba5ef24831bea4d7bfd
藤橋祐哉 <>
2023-09-23 13:04:25 +0900

    ローカルから公開

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1465d853a10e12f2642b3ae4f9c694912292e8ddf07dedc496d28d37c8cb5b9c0e15d6ec28278c1492a0bafe776f7ccdb9506c3a7f35fb79c48aff858d62e4fe'
